ba026dbcfc9496e07973917004714288e73247c3
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2008-02-24 07:55:27 +0300

    emacs: * iswitchb mode added * Fancy comments * Syntax highlihting moved to my-programing

--- emacs.d/my-programming.el
+++ emacs.d/my-programming.el
@@ -9,11 +9,27 @@
   (insert "#ifndef " title "\n#define " title "\n\n#endif /* " title " */\n" ))
 
 
+; turn on syna
+(global-font-lock-mode t)
+; Highlight parensthesis
+(show-paren-mode t) 
+; Highlight whole expression if it's not fully visible
+(setq show-paren-style 'mixed) 
+
+; Indentation
+(setq-default tab-width 4)
+(setq-default indent-tabs-mode nil)
+(setq c-default-style "bsd" c-basic-offset 4)
+
+;; ========================================================
+;; Define hooks 
 ;; Hooks
 (defun my-python-hooks ()
   (setq tab-width 4)
   )
 
+;; =========================================================
+;; Set hooks 
 (add-hook 'python-mode 'my-python-hooks)
 
 
