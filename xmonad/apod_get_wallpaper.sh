6c615fa17a0e3f3fa8a66f526b1bca4911abec3a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2008-03-16 21:52:19 +0300

    APOD - script greatly reworked

--- xmonad/apod_get_wallpaper.sh
+++ xmonad/apod_get_wallpaper.sh
@@ -2,70 +2,63 @@
 
 # apod_get_wallpaper (c) 2008 Alexey Khudyakov <alexey skladnoy {at} gmail com>
 
-# Simple script which downloads wallpaper from APOD
-# (http://antwrp.gsfc.nasa.gov/apod) and draws it on root window
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
 #
-# Not for people with expensive internet. One picture is about 1 MB.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 
-# Directory where APOD files lie 
-APOD_DIR=${HOME}/.share/apod
-# Link from which wallpaper was downloaded
-APOD_LINK=${APOD_DIR}/link
-# Wallpaper 
-APOD_PAPER=${APOD_DIR}/wallpaper.jpg
-# Description
-APOD_DESCR=${APOD_DIR}/description
+# Simple script which downloads wallpaper from APOD
+# (http:/apod.nasa.gov/apod) and draws it on root window
 
-# Flags
-DO_DOWNLOAD=
 
-## Test for APOD directory existence
-if [ ! -d $APOD_DIR ]; then
-    mkdir -p $APOD_DIR
+APOD_DIR=${HOME}/.share/apod         # Directory where APOD files lie 
+APOD_LINK=${APOD_DIR}/link           # Link from which wallpaper was downloaded
+APOD_PAPER=${APOD_DIR}/wallpaper.jpg # Wallpaper 
+APOD_DESCR=${APOD_DIR}/description   # Description
+APOD_HOST=http://apod.nasa.gov       # Web host of APOD 
+
+# Test for APOD directory existence
+[ -d $APOD_DIR ] || mkdir -p $APOD_DIR
+
+# Download apod page for parsing 
+TMP=$(mktemp)
+#TMP=~/qqq/wall/apod
+wget -q ${APOD_HOST}/apod/ -O - | \
+    awk '/^ *$/ { print; print; }  /[^ ]/ { printf("%s", $0); }' > $TMP 
+
+# Make explanation 
+sed -r $TMP \
+    -e '1,/Explanation:/ {/Credit & Copyright/ !d; s#</b>.*##; s/.*<b> *//;}' \
+    -e '/<script.*>/,$ d' \
+    -e 's#</?[^>]*>##g' | 
+sed -e '3,$ {/^ *$/ d}' | fmt > $APOD_DESCR
+
+# Extract picture link
+LINK=${APOD_HOST}/$(sed $TMP -r -e '/<IMG/ {s/.*<a href="(image[^"]*)".*/\1/; p}; d')
+
+# Download picture if needed 
+if  [ ! -f $APOD_PAPER  -o  ! -f $APOD_LINK  -o  "$1" = force ] || \
+    [ "$(cat $APOD_LINK)" != $LINK ] 
+then
+    echo $LINK > $APOD_LINK
+    wget  $LINK -O - | convert - -resize 1024x768 $APOD_PAPER
 fi
-
-## If wallpaper is not created today - set download flag
-if [ "$1" = force ]; then
-    DO_DOWNLOAD=yes
-elif [ ! -f $APOD_PAPER ]; then
-    DO_DOWNLOAD=yes
-elif [  $(date +%Y-%m-%d) != $(stat --format="%y" $APOD_PAPER | cut -c 1-10) ]; then
-    DO_DOWNLOAD=yes
-fi
-
-## If not downloaded - download
-if [ "$DO_DOWNLOAD" = "yes" ]; then
-    TMP=$(mktemp)
-    wget http://antwrp.gsfc.nasa.gov/apod/ -O $TMP
-
-    # Make explanation 
-    cat $TMP | 
-    sed -e '1,/<b> Explanation: <\/b>/ d' \
-        -e '/<center>/,$ d' |
-    tr "\n" ' ' |
-    sed -r \
-        -e 's/<\/?a[^>]*>//g' \
-        -e 's/ + / /g' \
-        -e 's/^ *//' > $APOD_DESCR
-    echo >> $APOD_DESCR
-
-    # Extract and download wallpaper    
-    LINK=$( echo -n http://antwrp.gsfc.nasa.gov/
-        sed $TMP -r \
-            -e '1,/<IMG SRC=/ !d' \
-            -e '/<h1>/,$ !d' \
-            -e '/<a.*image\// !d' \
-            -e 's/.*href="(.*)".*/\1/' )
-
-    # Download picture if needed 
-    if [ ! -f $APOD_LINK ] || [ "$(cat $APOD_LINK)" != $LINK ]; then
-        echo $LINK > $APOD_LINK
-        wget $LINK -O - | convert - -resize 1024x768 $APOD_PAPER
-    fi
-    rm -rf $TMP
-fi
+rm -rf $TMP
 
 # Show it!
-display -backdrop -background '#A0A0A0' \
-    -window root $APOD_PAPER
+display -backdrop -window root $APOD_PAPER
--- xmonad/apod_show_descritption.sh
+++ xmonad/apod_show_descritption.sh
@@ -1,30 +1,39 @@
 #!/bin/sh
 
-# apod_get_wallpaper (c) 2008 Alexey Khudyakov <alexey skladnoy {at} gmail com>
+# apod_show_description (c) 2008 Alexey Khudyakov <alexey skladnoy {at} gmail com>
+
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation files
+# (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 
 # Display description of APOD
 
 
-# Directory where APOD files lie 
-APOD_DIR=${HOME}/.share/apod
-# Description
-APOD_DESCR=${APOD_DIR}/description
+APOD_DIR=${HOME}/.share/apod        # Directory where APOD files lie 
+APOD_DESCR=${APOD_DIR}/description  # Description
 
-if [ -f $APOD_DESCR ]; then 
-    TMP=$(mktemp)
-    # Format text 
-    fmt -w 85 $APOD_DESCR | sed -e 's/^/^p\(10\)/' > $TMP
-    # Count lines
-    N_LINES=$(wc -l < $TMP)
+if [ -f $APOD_DESCR ]; then
+    N_LINES=$(wc -l < $APOD_DESCR)
 
-    # Some hardcoded display parameters
-    (
-        echo APOD
-        cat $TMP
-    ) |
-    dzen2 -p 60 -l $((N_LINES+1)) \
-        -e 'onstart=uncollapse;button1=exit:0' \
-        -w 624 -x 200 -y 200
-    
-    rm -rf $TMP
+    fmt -w 85 $APOD_DESCR | 
+    sed -e '3,$ s/^/^p(10)/' | 
+    dzen2 -p 60 -l $N_LINES \
+        -w 640 -x 192 -y 200 -bg '#444' \
+        -e 'onstart=uncollapse;button1=exit:0' 
 fi
