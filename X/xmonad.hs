727668dc0eb3c27131525f7171beb353dacac533
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2009-05-14 22:33:51 +0400

    xmonad: cosmetic changes

--- X/xmonad.hs
+++ X/xmonad.hs
@@ -169,14 +169,10 @@ myLayout = smartBorders $
            onWorkspace "IM" (IM.IM (1%5) (IM.Resource "main")) $
            tiled ||| Mirror tiled ||| Full
     where
-      -- default tiling algorithm partitions the screen into two panes
       tiled   = Tall nmaster delta ratio
-      -- The default number of windows in the master pane
-      nmaster = 1
-      -- Default proportion of screen occupied by master pane
-      ratio   = 1/2
-      -- Percent of screen to increment by when resizing panes
-      delta   = 3/100
+      nmaster = 1       -- The default number of windows in the master pane
+      ratio   = 1/2     -- Default proportion of screen occupied by master pane
+      delta   = 3/100   -- Percent of screen to increment by when resizing panes
  
 ------------------------------------------------------------------------
 -- Window rules:
