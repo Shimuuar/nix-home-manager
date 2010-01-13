b552f4ff3d9c6edef5ec7b7e309a860bf0a7735a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2010-01-13 20:59:06 +0300

    New abbrevs for emacs

--- emacs.d/my-abbrevs.el
+++ emacs.d/my-abbrevs.el
@@ -104,20 +104,75 @@
 (define-skeleton my-skel-haskell-LANG
   "Skeleton for haskell LANGUAGE pragma"
   ""
-  "{-# LANGUAGE " _ " #-}" 
+  "{-# LANGUAGE "
+  (completing-read "Language extension: "
+		   '("BangPatterns"              "CPP"
+		     "DeriveDataTypeable"        "EmptyDataDecls"
+		     "ExistentialQuantification" "FlexibleContexts"
+		     "FlexibleInstances"         "FunctionalDependencies"
+		     "GADTs"                     "GeneralizedNewtypeDeriving"
+		     "IncoherentInstances"       "ImpredicativeTypes"
+		     "KindSignatures"            "LiberalTypeSynonyms"
+		     "MagicHash"                 "MultiParamTypeClasses"
+		     "NamedFieldPuns"            "NoImplicitPrelude"
+		     "NoMonomorphismRestriction" "OverlappingInstances"
+		     "OverloadedStrings"         "PatternGuards"
+		     "PackageImports"            "Rank2Types"
+		     "RankNTypes"                "RecordWildCards"
+		     "StandaloneDeriving"        "ScopedTypeVariables"
+		     "TypeFamilies"              "TemplateHaskell"
+		     "TypeOperators"             "TypeSynonymInstances"
+		     "UndecidableInstances"      "ViewPatterns"
+		     ))
+  " #-}" 
   )
+(define-skeleton my-skel-haskell-import
+  "Skeleton for common haskell imports"
+  ""
+  "import "
+  (completing-read "Import module: "
+		   '("Control.Applicative"
+		     "Control.Arrow"
+		     "Control.Exception"
+		     "Control.Monad"
+		     "Control.Monad.Error"
+		     "Control.Monad.Reader"
+		     "Control.Monad.State"
+		     "Control.Monad.Trans"
+		     "Control.Monad.Writer"
+		     "Data.Accesor"
+		     "Data.Accesor.Template"
+		     "Data.Default"
+		     "Data.List"
+		     "Data.Maybe"
+		     "Data.Monoid"
+		     "Language.Haskell.TH"
+		     "Prelude"
+		     "System"
+		     "System.Directory"
+		     "System.IO"
+		     "System.Process"
+		     "Text.Printf"
+		     ; Module which usually imported qualified
+		     "qualified Data.Map as Map"
+		     "qualified Data.IntMap as IntMap"
+		     "qualified Data.Set as Set"
+		     )))
+
+
 (define-abbrev-table 'haskell-mode-abbrev-table
   '(
     ; Import
-    ("imp"  "import"    nil 0)
-    ("qua"  "qualified" nil 0)
+    ("imp"  "import"           nil                    0)
+    ("impq" "import qualified" nil                    0)
+    ("impa" ""                 my-skel-haskell-import 0)
     ; Shorhands
-    ("ret"  "return"       nil 0)
-    ("pr"   "print"        nil 0)
-    ("fi"   "fromIntegral" nil 0)
+    ("ret"  "return"           nil                    0)
+    ("pr"   "print"            nil                    0)
+    ("fi"   "fromIntegral"     nil                    0)
     ; Pragmas
-    ("pscc"  ""         my-skel-haskell-SCC  0)
-    ("plang" ""         my-skel-haskell-LANG 0)
+    ("pscc"  ""                my-skel-haskell-SCC    0)
+    ("plang" ""                my-skel-haskell-LANG   0)
     ))
 
 (provide 'my-abbrevs)
