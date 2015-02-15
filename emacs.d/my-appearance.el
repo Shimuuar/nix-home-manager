fc7e9f006d39d457f4a43972ba5b880347e4e316
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2015-02-15 17:18:08 +0300

    Clear up modeline

--- emacs.d/my-appearance.el
+++ emacs.d/my-appearance.el
@@ -39,33 +39,27 @@
 (custom-set-variables
  '(mode-line-format
    (quote ("%e"
-	   #("-" 0 1 (help-echo "mouse-1: select (drag to resize), mouse-2 = C-x 1, mouse-3 = C-x 0"))
 	   mode-line-mule-info
 	   mode-line-modified
 	   mode-line-frame-identification
 	   mode-line-buffer-identification
-	   #("   " 0 3 (help-echo "mouse-1: select (drag to resize), mouse-2 = C-x 1, mouse-3 = C-x 0"))
 	   mode-line-position
 	   (vc-mode vc-mode)
-	   #("  " 0 2 (help-echo "mouse-1: select (drag to resize), mouse-2 = C-x 1, mouse-3 = C-x 0"))
 	   mode-line-modes
-	   (which-func-mode ("" which-func-format #("--" 0 2 (help-echo "mouse-1: select (drag to resize), mouse-2 = C-x 1, mouse-3 = C-x 0"))))
-	   (global-mode-string
-	    (#("--" 0 2 (help-echo "mouse-1: select (drag to resize), mouse-2 = C-x 1, mouse-3 = C-x 0")) global-mode-string))
 	   ; Tab width display for C/C++ modes and indentation type for haskell mode
+	   "-- "
 	   (:eval (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode))
 		      (format "--[%s Tab:%i]" (if indent-tabs-mode "tabs" "ws") tab-width)
 		      ""
 		    ))
 	   (:eval (if (eq major-mode 'haskell-mode)
-		      (cond ((eq haskell-indentation-layout-offset 2) "--[MY]")
-			    ((eq haskell-indentation-layout-offset 4) "--[JOHAN]")
-			    ( t                                       "--[WTF!!!]")
+		      (cond ((eq haskell-indentation-layout-offset 2) "[MY]")
+			    ((eq haskell-indentation-layout-offset 4) "[JOHAN]")
+			    ( t                                       "[WTF!!!]")
 		       )
 		    ""))
-	   ; Rest
-	   #("-%-" 0 3 (help-echo "mouse-1: select (drag to resize), mouse-2 = C-x 1, mouse-3 = C-x 0")))
-	  )))
+	   )
+	   )))
 ;; ================
 
 
