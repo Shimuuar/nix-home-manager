1388d8dc9584630ceabcef9558330799a2ad3a15
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2016-02-08 20:36:52 +0300

    Add ability to work with pools

--- util/mdo.c
+++ util/mdo.c
@@ -12,10 +12,20 @@ void usage(void)
     exit(1);
 }
 
+void wait_for_pool(int pool_size) {
+    while( pool_size ) {
+        int status;
+        wait(&status);
+        pool_size--;
+    }
+}
+
 int main(int argc, char** argv)
 {
-    int start, end, len, i, status;
+    int start, end, len;
     char** command;
+    int max_pool  = 1;
+    int pool_size = 0;
 
     /* Find both "--" */
     for(start = 1;         start < argc && strcmp(argv[start],"--"); start++);
@@ -26,25 +36,34 @@ int main(int argc, char** argv)
     len = start + (argc - end) + 2;
     command = (char**)malloc( len * sizeof(char*) );
     /* Write command */
-    for(i = 1; i < start; i++)       /* Arguments before "--" */
+    for(int i = 1; i < start; i++)       /* Arguments before "--" */
         command[i] = argv[i];
-    for(i = 1; i < argc - end; i++ ) /* Arguments after "--" */
+    for(int i = 1; i < argc - end; i++ ) /* Arguments after "--" */
         command[i+start] = argv[end+i];
     command[start + argc - end + 1] = NULL; /* Terminating NULL */
     /* execute commands */
-    for(i = start+1; i < end; i++) {
+    for(int i = start+1; i < end; i++) {
         switch( fork() ) {
         case -1:                /* Cannot fork */
             perror("mdo: cannot fork");
+            wait_for_pool(pool_size);
             exit(1);
         case 0:                 /* Child */
             command[start] = argv[i];
             execvp(command[1], command+1);
             perror("mdo: cannot execute command: ");
+            wait_for_pool(pool_size);
             exit(1);
         default:                /* Parent */
-            wait(&status);
+            pool_size++;
+            if( pool_size >= max_pool ) {
+                int status;
+                wait(&status);
+                pool_size--;
+            }
         }
     }
+    // Wait for remaining processes.
+    wait_for_pool(pool_size);
     return 0;
 }
