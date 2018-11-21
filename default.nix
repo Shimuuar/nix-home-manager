5a1c96bd1c1f714614efee6a5759f8b14061360d
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2018-11-22 00:21:08 +0300

    Add ploly server

--- default.nix
+++ default.nix
@@ -5,19 +5,23 @@ let
     name             = "khudyakov-env";
     ignoreCollisions = true;
     paths = [
+      # Nix
+      self.nix-prefetch-git
       # GHC
-      self.haskell.compiler.ghc843
+      self.haskell.compiler.ghc844
       self.haskell.compiler.ghc802
       self.haskell.compiler.ghc822
-      self.haskell.compiler.ghc861
+      self.haskell.compiler.ghc862
       # Other haskell utils
       self.haskellPackages.cabal-install
       self.haskellPackages.stack
       self.haskellPackages.weeder
       self.haskellPackages.graphmod
+      self.hlint
       self.cabal2nix
       # My programs
       self.arxiv-get
+      self.plotly-server
       self.root-plot
       ];
     });
@@ -35,11 +39,13 @@ in
   fetchhgPrivate = self.callPackage ./pkgs/fetchhgPrivate {};
   root-plot      = self.callPackage ./pkgs/root-plot {};
   arxiv-get      = self.haskellPackages.callPackage ./pkgs/arxiv-get {};
+  plotly-server  = self.haskellPackages.callPackage ./pkgs/plotly-server {};
   # Override haskell stuff
   haskellPackages = pkgs.haskellPackages.override haskOverrides;
   haskell         = pkgs.haskell // {
     packages = pkgs.haskell.packages // {
-      ghc843 = pkgs.haskell.packages.ghc843.override haskOverrides;
+      ghc862 = pkgs.haskell.packages.ghc862.override haskOverrides;
+      ghc844 = pkgs.haskell.packages.ghc844.override haskOverrides;
     };
   };
   # Build environment
