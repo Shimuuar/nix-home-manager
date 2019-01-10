177428629aabc4ebc2347cc80c96bdf01552f251
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-01-10 20:42:27 +0300

    Add comic scraper

---
+++ pkgs/haskell/comic-scraper.nix
@@ -0,0 +1,19 @@
+{ mkDerivation, fetchhgPrivate, aeson, async, base, bytestring, containers, lens
+, network-uri, regex-applicative, spiderment, stdenv, text, time
+}:
+mkDerivation {
+  pname = "comic-scrapper";
+  version = "0.1";
+  src = fetchhgPrivate {
+    url = "ssh://hg@bitbucket.org/Shimuuar/comic-scraper";
+    rev = "9f97e87be657d0cf1d2a47f0d32639e229bfb504";
+  };
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    aeson async base bytestring containers lens network-uri
+    regex-applicative spiderment text time
+  ];
+  description = "Scrapper for comicss";
+  license = stdenv.lib.licenses.bsd3;
+}
