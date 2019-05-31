3f6657a299a512fe649a48f38d6cb96a3eb991cc
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-05-31 12:41:56 +0300

    Fix locale issue

--- lug.nix
+++ lug.nix
@@ -8,6 +8,9 @@ let
         if [ -e /home/alexey/.nix-profile/etc/profile.d/nix.sh ]; then
           . /home/alexey/.nix-profile/etc/profile.d/nix.sh;
         fi
+        # See https://github.com/NixOS/nixpkgs/issues/38991
+        export LOCALE_ARCHIVE_2_27=$(nix-build --no-out-link "<nixpkgs>" -A glibcLocales)/lib/locale/locale-archive
+        #
         export NIX_PATH=$HOME/.nix-defexpr/channels:$NIX_PATH
         export NIX_PATH=ssh-auth-sock=$SSH_AUTH_SOCK:$NIX_PATH
         export NIX_PATH=ssh-config-file=/etc/ssh/ssh_config:$NIX_PATH
