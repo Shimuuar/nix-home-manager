bf57a07941d1814c881ab35891553aaeedc915fa
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-10-09 01:18:09 +0300

    Add automatic tunnel to dagda

--- ogma.nix
+++ ogma.nix
@@ -6,6 +6,11 @@ let
       extraPrograms = ''
         systemctl --user start ssh-agent
         ${pkgs.lxpanel}/bin/lxpanel &
+        # Run tunnel to dagda
+        while : ; do
+          ssh -oBatchMode=yes -N -L 127.0.0.2:58846:127.0.0.1:58846 192.168.1.4
+          sleep 30
+        done &
         '';
     };
   };
