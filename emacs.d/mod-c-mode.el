6f45e62f03b3446673b758774e32cd8835328f9f
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-12-08 22:21:58 +0300

    Start moving abbreviations around

--- emacs.d/mod-c-mode.el
+++ emacs.d/mod-c-mode.el
@@ -3,6 +3,10 @@
 ;;; Tweaks for c-mode & c++-mode
 ;;;
 
+;; =========================================================
+;; Functions
+;; =========================================================
+
 (defun my/insert-guard(title)
   "Insert C/C++ header guards quickly"
   (interactive "sType guard name: ")
@@ -40,6 +44,73 @@ int main(int argc, char** argv)
     return 0;
 }
 "))
+
+
+;; =========================================================
+;; C/C++ abbreviations
+;; =========================================================
+(define-skeleton my/skel-reintepret-cast
+  "Skeleton for reinterpret cast"
+  "Typename: "
+  "reinterpret_cast<" str ">(" _ ")")
+(define-skeleton my/skel-static-cast
+  "Skeleton for static cast"
+  "Typename: "
+  "static_cast<" str ">(" _ ")")
+(define-skeleton my/skel-printf
+  "Skeleton for printf" ""
+  "printf(\"" _ "\");" )
+
+(define-abbrev-table 'c-mode-abbrev-table
+  '(
+    ; Preprocessor directives
+    ("inc" "#include"  nil 0)
+    ("def" "#define"   nil 0)
+    ; Control structures
+    ("sw"   "switch"   nil 0)
+    ("ca"   "case"     nil 0)
+    ("br"   "break"    nil 0)
+    ("wh"   "while"    nil 0)
+    ("ret"  "return"   nil 0)
+    ("cont" "continue" nil 0)
+    ; Types and modifiers
+    ("co" "const"      nil 0)
+    ("vo" "void"       nil 0)
+    ("bo" "bool"       nil 0)
+    ; printf
+    ("pr" ""           my/skel-printf 0)
+    ))
+(define-abbrev-table 'c++-mode-abbrev-table
+  '(
+    ; Preprocessor directives
+    ("inc" "#include"  nil 0)
+    ("def" "#define"   nil 0)
+    ; Control structures
+    ("sw"   "switch"   nil 0)
+    ("ca"   "case"     nil 0)
+    ("br"   "break"    nil 0)
+    ("wh"   "while"    nil 0)
+    ("ret"  "return"   nil 0)
+    ("cont" "continue" nil 0)
+    ; Types and modifiers
+    ("co" "const"      nil 0)
+    ("vo" "void"       nil 0)
+    ("bo" "bool"       nil 0)
+    ; Classes
+    ("pub"  "public"   nil 0)
+    ("priv" "private"  nil 0)
+    ; printf
+    ("pr" ""           my/skel-printf 0)
+    ; Casts
+    ("rcast" ""       my/skel-reintepret-cast 0)
+    ("scast" ""       my/skel-static-cast 0)
+    ; STL
+    ("cout"  "std::cout" nil 0)
+    ("cerr"  "std::cerr" nil 0)
+    ("endl"  "std::endl" nil 0)
+    ))
+
+
 
 ;; ===============================================
 ;; Indentation
--- emacs.d/my-abbrevs.el
+++ emacs.d/my-abbrevs.el
@@ -3,69 +3,6 @@
 ;;; Abbreviations
 ;;;
 
-;; =========================================================
-;; C/C++ abbreviations
-;; =========================================================
-(define-skeleton my/skel-reintepret-cast
-  "Skeleton for reinterpret cast"
-  "Typename: "
-  "reinterpret_cast<" str ">(" _ ")")
-(define-skeleton my/skel-static-cast
-  "Skeleton for static cast"
-  "Typename: "
-  "static_cast<" str ">(" _ ")")
-(define-skeleton my/skel-printf
-  "Skeleton for printf" ""
-  "printf(\"" _ "\");" )
-  
-(define-abbrev-table 'c-mode-abbrev-table 
-  '( 
-    ; Preprocessor directives 
-    ("inc" "#include"  nil 0)
-    ("def" "#define"   nil 0)
-    ; Control structures
-    ("sw"   "switch"   nil 0)
-    ("ca"   "case"     nil 0)
-    ("br"   "break"    nil 0)
-    ("wh"   "while"    nil 0)
-    ("ret"  "return"   nil 0)
-    ("cont" "continue" nil 0)
-    ; Types and modifiers 
-    ("co" "const"      nil 0)
-    ("vo" "void"       nil 0)
-    ("bo" "bool"       nil 0)
-    ; printf
-    ("pr" ""           my/skel-printf 0)
-    ))
-(define-abbrev-table 'c++-mode-abbrev-table
-  '(
-    ; Preprocessor directives 
-    ("inc" "#include"  nil 0)
-    ("def" "#define"   nil 0)
-    ; Control structures
-    ("sw"   "switch"   nil 0)
-    ("ca"   "case"     nil 0)
-    ("br"   "break"    nil 0)
-    ("wh"   "while"    nil 0)
-    ("ret"  "return"   nil 0)
-    ("cont" "continue" nil 0)
-    ; Types and modifiers 
-    ("co" "const"      nil 0)
-    ("vo" "void"       nil 0)
-    ("bo" "bool"       nil 0)
-    ; Classes
-    ("pub"  "public"   nil 0)
-    ("priv" "private"  nil 0)
-    ; printf
-    ("pr" ""           my/skel-printf 0)
-    ; Casts
-    ("rcast" ""       my/skel-reintepret-cast 0)
-    ("scast" ""       my/skel-static-cast 0)
-    ; STL
-    ("cout"  "std::cout" nil 0)
-    ("cerr"  "std::cerr" nil 0)
-    ("endl"  "std::endl" nil 0)
-    ))
 ;; =================
 
 
