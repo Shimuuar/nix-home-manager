1413fffec51952e384e347c8393ba3cfddec1c93
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2019-12-31 16:02:05 +0300

    Fix root-plot

--- overlays/khudyakov/pkgs/root-plot/default.nix
+++ overlays/khudyakov/pkgs/root-plot/default.nix
@@ -1,10 +1,11 @@
-{ stdenv, fetchhg, root, boost, bison, flex }:
+{ stdenv, fetchgit, root, boost, bison, flex }:
 stdenv.mkDerivation {
   name    = "root-plot";
   version = 0.1;
-  src = fetchhg {
-    url = "https://bitbucket.org/Shimuuar/root-plot";
-    rev = "9333e13c4e7cf3ece65675ecf75c46033bfd3b62";
+  src = fetchgit {
+    url    = "https://Shimuuar@bitbucket.org/Shimuuar/root-plot.git";
+    rev    = "38299765689e0217fd4c3bab17b832f992c47e5c";
+    sha256 = "183kf6gvpng6gb8kkfnhscr7vv04wb2wxif7vgjsv4y2qy55kdyi";
   };
   nativeBuildInputs = [ bison flex ];
   buildInputs       = [ stdenv root boost ];
