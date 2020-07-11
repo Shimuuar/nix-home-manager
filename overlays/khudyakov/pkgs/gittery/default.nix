6bb0ae49c67733aec37f3ce4dbba123514fc4bcc
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-07-11 14:27:44 +0300

    Use plain fetchgit

--- overlays/khudyakov/pkgs/gittery/default.nix
+++ overlays/khudyakov/pkgs/gittery/default.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, fetchgitPrivate
+{ mkDerivation, fetchgit
 , aeson, base, directory, filepath, hostname, containers
 , optparse-applicative, process, stdenv, text, transformers
 , unordered-containers, yaml
@@ -6,7 +6,7 @@
 mkDerivation {
   pname = "gittery";
   version = "0.1";
-  src = fetchgitPrivate {
+  src = fetchgit {
     url    = "git@bitbucket.org:Shimuuar/gittery.git";
     rev    = "5e23356110ddda5cf2dcfa1893a0ed27e4fdf4aa";
     sha256 = "1yv14nk0aingam0ba788kwqr7fma1zyp08x4py1zkf0mqmiyk315";
