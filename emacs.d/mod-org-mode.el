a08dc75645969e143d50739da9c82066f52ff4da
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-09-12 20:12:45 +0300

    Drop RC from org-mode

--- emacs.d/mod-org-mode.el
+++ emacs.d/mod-org-mode.el
@@ -4,7 +4,7 @@
 ;;;
 
 (setq org-todo-keywords
-      '((sequence "TODO" "PROG" "RC" "BLOCKED" "|" "DONE" "DROPPED")))
+      '((sequence "TODO" "PROG" "BLOCKED" "|" "DONE" "DROPPED")))
 
 ; FIXME: is this a right way?
 (custom-set-variables
@@ -12,6 +12,6 @@
    (quote
     ((org-todo-keyword-faces
       ("BLOCKED" . "red")
-      ("RC"      . "yellow"))))))
+      )))))
 
 (provide 'mod-org-mode)
