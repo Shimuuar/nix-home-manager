890188e3831ff4aab46711a7cbf80d41032788dd
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-09-12 20:40:03 +0300

    Use local copy

--- cfg/default.nix
+++ cfg/default.nix
@@ -2,11 +2,7 @@ params:
 { config, pkgs, ... }:
 let
   # Repository with config files
-  cfg = builtins.fetchGit {
-    url = "https://github.com/Shimuuar/config.git";
-    ref = "master";
-    rev = "ec750c063ccaac7a1a0d4a7bc88af44cd8ab0986";
-  };
+  cfg = ../config;
 in
 {
    imports = [
