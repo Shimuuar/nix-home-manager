97acb4132cd19bd2fbd54f04ae9a1de4c5bb1564
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-09-13 13:59:33 +0300

    Set up agenda for emacs

--- config/emacs.d/mod-org-mode.el
+++ config/emacs.d/mod-org-mode.el
@@ -6,15 +6,22 @@
 (setq org-todo-keywords
       '((sequence "TODO" "PROG" "BLOCKED" "|" "DONE" "DROPPED")))
 
-; FIXME: is this a right way?
+;; FIXME: is this a right way?
 (custom-set-variables
   '(safe-local-variable-values
    (quote
     ((org-todo-keyword-faces
       ("BLOCKED" . "red")
       )))))
+;; Set up agenda files
 (add-hook 'org-mode-hook (lambda ()
   (local-set-key (kbd "C-c d") (lambda () (interactive) (org-todo "DONE")))
+  (setq org-agenda-files
+	'("~/data/tracker/todo.org"
+	  "~/data/tracker/realty.org"
+	  "~/data/tracker/work.org"
+	  "~/data/tracker/trading.org"
+	  ))
   ))
 
 (provide 'mod-org-mode)
