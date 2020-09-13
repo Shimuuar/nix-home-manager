96a319e2fb32ab875bf6da16f532c2415ac00619
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-09-13 14:46:48 +0300

    Set up layout corretly

--- config/emacs-todo.el
+++ config/emacs-todo.el
@@ -2,3 +2,7 @@
 (find-file "~/data/tracker/work.org")
 (find-file "~/data/tracker/todo.org")
 (org-agenda nil "a")
+;; Set up layout correctly. Very clunky
+(delete-other-windows)
+(split-window-right)
+(switch-to-buffer "todo.org")
