2d0c4bf6f63c9bff245749960ef5418763e7e1db
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2020-09-16 00:06:34 +0300

    Tweak org-mode a bit

--- config/emacs.d/mod-org-mode.el
+++ config/emacs.d/mod-org-mode.el
@@ -13,15 +13,26 @@
     ((org-todo-keyword-faces
       ("BLOCKED" . "red")
       )))))
+
+;; Customize org-mode
+(setq org-agenda-start-on-weekday 1)
+(setq calendar-week-start-day     1)
+(setq org-agenda-files
+      '("~/data/tracker/todo.org"
+	"~/data/tracker/realty.org"
+	"~/data/tracker/work.org"
+	"~/data/tracker/trading.org"
+	))
+
 ;; Set up agenda files
 (add-hook 'org-mode-hook (lambda ()
+  ;; Key bindings
   (local-set-key (kbd "C-c d") (lambda () (interactive) (org-todo "DONE")))
-  (setq org-agenda-files
-	'("~/data/tracker/todo.org"
-	  "~/data/tracker/realty.org"
-	  "~/data/tracker/work.org"
-	  "~/data/tracker/trading.org"
-	  ))
+  ;; Delete trailing space on save
+  (add-hook 'before-save-hook
+	    (lambda () (delete-trailing-whitespace))
+	    nil
+	    'local)
   ))
 
 (provide 'mod-org-mode)
