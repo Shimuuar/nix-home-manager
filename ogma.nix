3e9f3309ec880d839d61dfbb83e9deeee25eefc1
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2021-08-02 09:21:44 +0300

    Xrandr incantation for 4k monitor

--- ogma.nix
+++ ogma.nix
@@ -6,6 +6,9 @@ let
       extraPrograms = ''
         # Turn on num lock
         ${pkgs.numlockx}/bin/numlockx
+        xrandr \
+          --output HDMI-A-0 --mode 1920x1080 --pos 0x0 --rotate normal --scale 2x2 \
+          --output HDMI-A-1 --mode 3840x2160 --pos 3840x0
         # Run tunnel to dagda (Deluge)
         while : ; do
           ssh -oBatchMode=yes -N -L 127.0.0.2:58846:127.0.0.1:58846 192.168.1.4
