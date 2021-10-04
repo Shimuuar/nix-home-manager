530d2138e6bb9d21db58748c59e6dc86e18b258a
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2021-10-04 15:55:26 +0300

    Split X stuff

--- cfg/default.nix
+++ cfg/default.nix
@@ -3,19 +3,6 @@ params:
 let
   # Repository with config files
   cfg = ../config;
-  # Wrapped telegram. It oesn;t like XDG_CURRENT_DESKTOP set
-  telegram-wrapped = pkgs.stdenv.mkDerivation {
-    name    = "telegram-desktop";
-    builder = pkgs.writeScript "telegram-builder" ''
-      ${pkgs.coreutils}/bin/mkdir -p $out/bin
-      ${pkgs.coreutils}/bin/cat > $out/bin/telegram-wrapped <<EOF
-      #!${pkgs.stdenv.shell}
-      unset XDG_CURRENT_DESKTOP
-      ${pkgs.tdesktop}/bin/telegram-desktop "\$@"
-      EOF
-      ${pkgs.coreutils}/bin/chmod +x $out/bin/telegram-wrapped
-      '';
-  };
 in
 {
    imports = [
@@ -88,75 +75,10 @@ in
     haskellPackages.ghc-prof-flamegraph
     haskellPackages.graphmod
     haskellPackages.hasktags
-    haskellPackages.hasktags
     haskellPackages.stack
-    haskellPackages.weeder
     hlint
-    # ----------------
-    # Window magament
-    dmenu
-    xdotool
-    wmctrl
-    spectacle
-    libnotify
-    # ----------------
-    # GUI programs
-    arandr
-    chromium
-    chromium-temp
-    darktable
-    djview
-    deluge
-    evince
-    firefox
-    krusader
-    gimp
-    nomacs
-    gwenview
-    libreoffice
-    telegram-wrapped
-    thunderbird
-    wireshark
-    xournal
-    xorg.xkill
-    xorg.xdpyinfo
-    xterm
-    freecad
-    cura
+
     idea.idea-community
-    # ----------------
-    # KDE
-    breeze-icons
-    hicolor-icon-theme
-    kcharselect
-    plasma5Packages.dolphin
-    plasma5Packages.dolphin-plugins
-    plasma5Packages.kate
-    plasma5Packages.konsole
-    plasma5Packages.okular
-    plasma5Packages.systemsettings
-    plasma5Packages.breeze-gtk
-    plasma5Packages.breeze-qt5
-    plasma5Packages.breeze-grub
-    plasma5Packages.breeze-plymouth
-    plasma5Packages.oxygen
-    plasma5Packages.plasma-integration
-    plasma5Packages.oxygen
-    # ----------------
-    # media
-    feh
-    mpv
-    pavucontrol
-    # ----------------
-    # Fonts
-    unifont
-    terminus_font
-    terminus_font_ttf
-    # ----------------
-    stalonetray
-    xmobar
-    gnome3.networkmanagerapplet
-    xfce.xfce4-power-manager
   ];
   #
   fonts.fontconfig.enable = params.fontconfig or false;
@@ -170,50 +92,6 @@ in
     publicshare = "$HOME/Desktop/Public";
     templates   = "$HOME/Desktop/Templates";
     videos      = "$HOME/Desktop/Videos";
-  };
-  # GTK
-  # FIXME: doesn't work due to dconf errors
-#  gtk = {
-#    enable = true;
-#    iconTheme = {
-#      name    = "Breeze-Dark";
-#      package = pkgs.breeze-icons;
-#    };
-#    theme = {
-#      name    = "Breeze-Dark";
-#      package = pkgs.breeze-gtk;
-#    };
-#  };
-  # Qt
-  qt = {
-    enable = true;
-    style  = {
-      name    = "Breeze-Dark";
-      package = pkgs.breeze-qt5;
-    };
-  };
-  # ----
-  programs.lxpanel = {
-    enable  = true;
-    edge    = "top";
-    plugins = [
-      { type = "deskno"; }
-      { type = "taskbar";
-        config = {
-          ShowAllDesks="0";
-        };
-      }
-      { type = "batt";
-        config = {
-          HideIfNoBattery = "1";
-        };
-      }
-      { type = "cpufreq"; }
-      { type = "thermal"; }
-      { type = "tray";    }
-      { type = "cpu";     }
-      { type = "dclock";  }
-    ];
   };
   # ----
   programs.aspell = {
@@ -375,27 +253,6 @@ in
   services.gpg-agent = {
     enable = true;
   };
-  # ----
-  # Surprisingly nm-applet doesn't work without notification service
-  services.dunst = {
-    enable = true;
-    settings = {
-      global = {
-        geometry           = "400x20-30+50";
-        frame_color        = "#eceff1";
-        font               = "Droid Sans 12";
-        padding            = 2;
-        horizontal_padding = 5;
-        frame_width        = 1;
-        separator_heights  = 4;
-      };
-      urgency_normal = {
-        background = "#37474f";
-        foreground = "#eceff1";
-        timeout    = 10;
-      };
-    };
-  };
   # ---- X session ----
   xsession = {
     enable    = true;
@@ -417,7 +274,7 @@ in
       # Programs
       ${pkgs.firefox}/bin/firefox                                         &
       ${pkgs.thunderbird}/bin/thunderbird                                 &
-      ${telegram-wrapped}/bin/telegram-wrapped                            &
+      telegram-wrapped                                                    &
       ${pkgs.udiskie}/bin/udiskie --tray                                  &
       emacs --name emacs-todo --eval '(load-file "${cfg}/emacs-todo.el")' &
       nix-shell -p 'texlive.combine {inherit (texlive) scheme-medium;}'   \
--- cfg/desktop.nix
+++ cfg/desktop.nix
@@ -1,5 +1,149 @@
 { config, pkgs, ... }:
+let
+  # Wrapped telegram. It oesn;t like XDG_CURRENT_DESKTOP set
+  telegram-wrapped = pkgs.stdenv.mkDerivation {
+    name    = "telegram-desktop";
+    builder = pkgs.writeScript "telegram-builder" ''
+      ${pkgs.coreutils}/bin/mkdir -p $out/bin
+      ${pkgs.coreutils}/bin/cat > $out/bin/telegram-wrapped <<EOF
+      #!${pkgs.stdenv.shell}
+      unset XDG_CURRENT_DESKTOP
+      ${pkgs.tdesktop}/bin/telegram-desktop "\$@"
+      EOF
+      ${pkgs.coreutils}/bin/chmod +x $out/bin/telegram-wrapped
+      '';
+  };
+in
 {
     home.packages = with pkgs; [
+    # ----------------
+    # Window magament
+    dmenu
+    xdotool
+    wmctrl
+    spectacle
+    libnotify
+    # ----------------
+    # GUI programs
+    arandr
+    chromium
+    chromium-temp
+    darktable
+    djview
+    deluge
+    evince
+    firefox
+    krusader
+    gimp
+    nomacs
+    gwenview
+    libreoffice
+    telegram-wrapped
+    thunderbird
+    wireshark
+    xournal
+    xorg.xkill
+    xorg.xdpyinfo
+    xterm
+    freecad
+    cura
+    # ----------------
+    # KDE
+    breeze-icons
+    hicolor-icon-theme
+    kcharselect
+    plasma5Packages.dolphin
+    plasma5Packages.dolphin-plugins
+    plasma5Packages.kate
+    plasma5Packages.konsole
+    plasma5Packages.okular
+    plasma5Packages.systemsettings
+    plasma5Packages.breeze-gtk
+    plasma5Packages.breeze-qt5
+    plasma5Packages.breeze-grub
+    plasma5Packages.breeze-plymouth
+    plasma5Packages.oxygen
+    plasma5Packages.plasma-integration
+    plasma5Packages.oxygen
+    # ----------------
+    # media
+    feh
+    mpv
+    pavucontrol
+    # ----------------
+    # Fonts
+    unifont
+    terminus_font
+    terminus_font_ttf
+    # ----------------
+    stalonetray
+    xmobar
+    gnome3.networkmanagerapplet
+    xfce.xfce4-power-manager
     ];
+  # GTK
+  # FIXME: doesn't work due to dconf errors
+#  gtk = {
+#    enable = true;
+#    iconTheme = {
+#      name    = "Breeze-Dark";
+#      package = pkgs.breeze-icons;
+#    };
+#    theme = {
+#      name    = "Breeze-Dark";
+#      package = pkgs.breeze-gtk;
+#    };
+#  };
+  # Qt
+  qt = {
+    enable = true;
+    style  = {
+      name    = "Breeze-Dark";
+      package = pkgs.breeze-qt5;
+    };
+  };
+  # ----
+  programs.lxpanel = {
+    enable  = true;
+    edge    = "top";
+    plugins = [
+      { type = "deskno"; }
+      { type = "taskbar";
+        config = {
+          ShowAllDesks="0";
+        };
+      }
+      { type = "batt";
+        config = {
+          HideIfNoBattery = "1";
+        };
+      }
+      { type = "cpufreq"; }
+      { type = "thermal"; }
+      { type = "tray";    }
+      { type = "cpu";     }
+      { type = "dclock";  }
+    ];
+  };
+  # ----
+  # Surprisingly nm-applet doesn't work without notification service
+  services.dunst = {
+    enable = true;
+    settings = {
+      global = {
+        geometry           = "400x20-30+50";
+        frame_color        = "#eceff1";
+        font               = "Droid Sans 12";
+        padding            = 2;
+        horizontal_padding = 5;
+        frame_width        = 1;
+        separator_heights  = 4;
+      };
+      urgency_normal = {
+        background = "#37474f";
+        foreground = "#eceff1";
+        timeout    = 10;
+      };
+    };
+  };
 }
