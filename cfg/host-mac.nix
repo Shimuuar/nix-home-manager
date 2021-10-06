b26762bb31f367059d878ad965ba5ef3ca3f861e
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2021-10-06 11:06:40 +0300

    Set locales correctly

--- cfg/host-mac.nix
+++ cfg/host-mac.nix
@@ -12,7 +12,10 @@ in
   ];
   extra-param.isMac = true;
   programs.bash.profileExtra = ''
+    # Set up nix
     . /Users/alexeykhudyakov/.nix-profile/etc/profile.d/nix.sh
-    # We want to use proper bash and not one supplied by apple
+    # Set up locales
+    export LANG=en_US.UTF-8
+    export LC_ALL=en_US.UTF-8
     '';
 }
