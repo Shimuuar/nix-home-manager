2d583503aa8b7b3fa868cf0a793a176155698a93
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2022-12-16 17:49:16 +0300

    Move configs

--- lug.nix
+++ lug.nix
@@ -1,1 +1,27 @@
-import ./cfg/host-lug.nix
+{ config, pkgs, ... }:
+let
+  # Repository with config files
+  cfg = ../config;
+in
+{
+  imports = [
+    ./cfg/common.nix
+    ./cfg/cli.nix
+    ./cfg/extra-cli.nix
+    #
+    ./cfg/desktop.nix
+    ./cfg/haskell.nix
+    #./cfg/idea.nix
+  ];
+  home.stateVersion  = "22.11";
+  home.username      = "alexey";
+  home.homeDirectory = "/home/alexey";
+  #
+  fonts.fontconfig.enable = true;
+  #
+  extra-param.composeKey    = "menu";
+  extra-param.extraXSession = ''
+    # Programs
+    xfce4-power-manager &
+    '';
+}
--- ogma.nix
+++ ogma.nix
@@ -1,1 +1,37 @@
-import ./cfg/host-ogma.nix
+{ config, pkgs, ... }:
+let
+  # Repository with config files
+  cfg = ../config;
+in
+{
+  imports = [
+    ./cfg/common.nix
+    ./cfg/cli.nix
+    ./cfg/extra-cli.nix
+    #
+    ./cfg/desktop.nix
+    ./cfg/haskell.nix
+    #./cfg/idea.nix
+  ];
+  home.stateVersion  = "22.11";
+  home.username      = "alexey";
+  home.homeDirectory = "/home/alexey";
+  #
+  fonts.fontconfig.enable = true;
+  #
+  extra-param.composeKey    = "ralt";
+  extra-param.extraXSession = ''
+    # Turn on num lock
+    ${pkgs.numlockx}/bin/numlockx
+    # Run tunnel to dagda (Deluge)
+    while : ; do
+      ssh -oBatchMode=yes -N -L 127.0.0.2:58846:127.0.0.1:58846 192.168.1.4
+      sleep 30
+    done &
+    # Run tunnel to dagda (syncthing)
+    while : ; do
+      ssh -oBatchMode=yes -N -L 127.0.0.2:8384:127.0.0.1:8384 192.168.1.4
+      sleep 30
+    done &
+    '';
+}
