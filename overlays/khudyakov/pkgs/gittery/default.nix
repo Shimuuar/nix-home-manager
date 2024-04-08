98e68744b34f03807cfa84b092dde8e3afb976da
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2024-04-08 11:54:33 +0300

    Oh shit! Here we go again

--- overlays/khudyakov/pkgs/gittery/default.nix
+++ overlays/khudyakov/pkgs/gittery/default.nix
@@ -7,7 +7,7 @@ mkDerivation {
   version = "0.1";
   src = builtins.fetchGit {
     url    = "ssh://git@sepulcarium.org/home/git/GIT/private-projects/gittery";
-    rev    = "f642d8d61f99431b9ac43507b57976f1ef5b1b8e";
+    rev    = "6d2f13bd406d882222d73dd11dfc982e01f58ca5";
     ref    = "master";
   };
   isLibrary = false;
