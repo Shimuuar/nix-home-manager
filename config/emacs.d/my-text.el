eb354441547bdcbed6a51a897ead87d05dd20d1b
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2024-06-17 20:46:05 +0300

    Add formatter

--- config/emacs.d/my-text.el
+++ config/emacs.d/my-text.el
@@ -10,6 +10,29 @@
   (ispell-change-dictionary (if (string= "ru" ispell-dictionary)
                                 "en" "ru")))
 
+(defun my/format-for-telegram()
+  "Format text for pasting to telegram/any other messenger"
+  (interactive)
+  (if (use-region-p)
+      (let ((oldbuf (current-buffer))
+	    (buf    (generate-new-buffer " clean-org"))
+	    )
+	(save-current-buffer
+	  (copy-to-buffer buf (region-beginning) (region-end))
+	  (set-buffer buf)
+ 	  ;; Clean up spaces
+	  (replace-regexp "^ *" "" nil (point-min) (point-max))
+	  (replace-regexp " *$" "" nil (point-min) (point-max))
+	  ;; Make paragraphs 1-lines. Hack arguably
+	  (set-fill-column 10000)
+	  (fill-region (point-min) (point-max))
+ 	  ;; Copy to kill-ring
+	  (kill-ring-save (point-min) (point-max))
+	  (kill-buffer buf)
+	  ))
+    (message "foofoo: No selection")
+    ))
+
 
 ;; ================================================================
 ;; Global settings
