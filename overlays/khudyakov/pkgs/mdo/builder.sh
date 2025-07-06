6259b84c0588886f2de0e3c52dd894831fda2589
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2025-07-06 14:21:51 +0300

    Fix mdo recipe

--- overlays/khudyakov/pkgs/mdo/builder.sh
+++ overlays/khudyakov/pkgs/mdo/builder.sh
@@ -1,6 +1,6 @@
 source $stdenv/setup
 
-cp $src/util/{Makefile,mdo.c} .
+cp $src/{Makefile,mdo.c} .
 make mdo
 mkdir -p $out/bin
 install mdo $out/bin/mdo
--- overlays/khudyakov/pkgs/mdo/default.nix
+++ overlays/khudyakov/pkgs/mdo/default.nix
@@ -1,10 +1,6 @@
 {stdenv}:
 stdenv.mkDerivation {
-  name = "mdo-0.1";
-  src  = builtins.fetchGit {
-    url = "https://github.com/Shimuuar/config.git";
-    ref = "master";
-    rev = "ec750c063ccaac7a1a0d4a7bc88af44cd8ab0986";
-  };
+  name    = "mdo-0.1";
+  src     = ../../../../config/util;
   builder = ./builder.sh;
 }
